8e386ec19f0803b85e1ce1ebd5cc6ab24f91b4c6
Carolina Duarte <carolcrduarte@knights.ucf.edu>
2021-09-13 20:28:20 -0400

    Initial commit

---
+++ src/main/java/Exercise22.java
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Carolina Duarte
+ *
+ */
+
+import java.util.Scanner;
+
+public class Exercise22 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int first_n = input.nextInt();
+        System.out.print("Enter the second number: ");
+        int second_n = input.nextInt();
+        System.out.print("Enter the third number: ");
+        int third_n = input.nextInt();
+
+        if (first_n > second_n && first_n > third_n)
+        {
+            System.out.print("The largest number is " + first_n + ".");
+        }
+
+        if (second_n > first_n && second_n > third_n)
+        {
+            System.out.print("The largest number is " + second_n + ".");
+        }
+
+        if (third_n > first_n && third_n > second_n)
+        {
+            System.out.print("The largest number is " + third_n + ".");
+        }
+
+    }
+}
